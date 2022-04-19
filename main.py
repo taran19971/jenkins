9985ed5390aa03021d0a7d1f54c618520722145d
taran19971 <59025984+taran19971@users.noreply.github.com>
2022-04-19 18:22:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-import module1
+from module1import SampleClass
 
 print('Hello World!')
+
+keith = SampleClass('Keith Dorgan')
+
+if keith.getName() != "Keith Morgan":
+  keith.setName("Keith Morgan")
+
+keith.greet()
+
+print("Keith is a:")
+keith.getSpecies()
