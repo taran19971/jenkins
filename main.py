b7ec3aeea65daf20ea391fe5cb179374349be0a4
taran19971 <59025984+taran19971@users.noreply.github.com>
2022-04-19 18:33:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ keith = SampleClass('Keith Dorgan', 'Human')
 if keith.getName() != "Keith Morgan":
   keith.setName("Keith Morgan")
 
-keith.greet()
-
 print("Keith is a: {0}".format(keith.getSpecies()))
+  
+keith.greet()
