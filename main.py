fe48a33d644623ad4df176aef94bf789af5e00d6
taran19971 <59025984+taran19971@users.noreply.github.com>
2022-04-20 18:49:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from module1import SampleClass
+# from module1 import SampleClass
 
-print('Hello World!')
+# print('Hello World!')
 
-keith = SampleClass('Keith Dorgan', 'Human')
+# keith = SampleClass('Keith Dorgan', 'Human')
 
-if keith.getName() != "Keith Morgan":
-  keith.setName("Keith Morgan")
+# if keith.getName() != "Keith Morgan":
+#   keith.setName("Keith Morgan")
 
-print("Keith is a: {0}".format(keith.getSpecies()))
+# print("Keith is a: {0}".format(keith.getSpecies()))
   
-keith.greet()
+# keith.greet()
+import module1
+
+print(dir(module1))
