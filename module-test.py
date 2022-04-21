14eb3991fc0f0a8cebd41c8c02a662e6ef4ac365
taran19971 <59025984+taran19971@users.noreply.github.com>
2022-04-21 18:27:50 +0530

    Update module-test.py

--- module-test.py
+++ module-test.py
@@ -16,11 +16,10 @@ class AllUnitTests(unittest.TestCase):
   def test_valid_return_type(self):
     self.assertTrue(self.sampleinstance.getName() != None)
     self.assertTrue(self.sampleinstance.getSpecies() != None)
-    self.assertTrue(self.sampleinstance.greet() != None)
+    self.assertFalse(self.sampleinstance.greet() != None)
     
-  def tearDown(self):
-    self.sampleinstance.dispose()
+#   def tearDown(self):
+#     self.sampleinstance.dispose()
    
 if __name__ == '__main__':
   unittest.main()
-    
